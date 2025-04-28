499ef7c75804d55fa8273b218f4013ab192f8a3b
RyanMConstable <RyanMConstable@gmail.com>
2025-04-27 20:24:54 -0400

    Adding to the cron check, mostly for checking github actions

--- cron/clashCurl.py
+++ cron/clashCurl.py
@@ -12,9 +12,14 @@ player = requests.get(url, headers=HEADERS).json()
 #This gets the current clanwar information
 url = f'https://api.clashofclans.com/v1/clans/{encodedClanTag}/currentwar'
 clan = requests.get(url, headers=HEADERS).json()
+state = clan["state"]
 
 #print(player)
 print(clan)
 #If we're checking daily we can grab this and add it to the database
-if clan["state"] == "inWar" or clan["state"] == "warEnded":
+if state == "inWar" or state == "warEnded":
     uniqueIndex = f"{clan['clan']['tag']}{clan['startTime'].split('T')[0]}"
+    if state == "warEnded":
+        print("Add each player attack to a table with player tag and clan tag")
+    else:
+        print("Monitor for updates, check against current info and update db in real time")
