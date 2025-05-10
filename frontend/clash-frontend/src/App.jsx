137e34142db30c3c45b24db3069a180482b54090
root <root@LAPTOP-O879DIR7.>
2025-05-10 15:04:39 -0400

    Updated frontend to temporary signup

--- frontend/clash-frontend/src/App.jsx
+++ frontend/clash-frontend/src/App.jsx
@@ -3,31 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function MyComponent({ inputLabel = '' } ) {
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+      setText(event.target.value);
+  };
+
+  return (
+    <div>
+      {inputLabel && <label htmlFor="inputBox">{inputLabel}</label>}
+      <input
+	id="inputBox"
+        type="text"
+        value={text}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="SignupInfo">
+	<p><MyComponent inputLabel="Player Tag "/></p>
+	<p><MyComponent inputLabel="Phone Number "/></p>
+	<p><MyComponent inputLabel="Password "/></p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
