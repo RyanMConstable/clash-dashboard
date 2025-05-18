2ddab0919117ac56106d0f99014048de032a1a62
RyanMConstable <RyanMConstable@gmail.com>
2025-05-18 16:57:08 -0400

    Finalized player table schema

--- db/01-init.sql
+++ db/01-init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS userinfo (
 );
 
 CREATE TABLE IF NOT EXISTS playerwarattacks (
-  id SERIAL PRIMARY KEY, playertag VARCHAR(15) NOT NULL, clantag VARCHAR(15) NOT NULL, time TIMESTAMP NOT NULL, defendertag VARCHAR(15) NOT NULL, stars VARCHAR(1), destructionpercentage VARCHAR(5), theorder VARCHAR(5), duration VARCHAR(5)
+  id VARCHAR(75) PRIMARY KEY, townhalllevel INTEGER, mapposition INTEGER, tag VARCHAR(15), defendertag VARCHAR(15), stars INTEGER, destructionpercentage FLOAT, ordernum INTEGER, duration INTEGER 
 );
 
 CREATE TABLE IF NOT EXISTS clanwars (
