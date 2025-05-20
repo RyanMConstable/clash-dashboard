b00995219490f4f53554c88b947d828b7fe7650b
RyanMConstable <RyanMConstable@gmail.com>
2025-05-20 14:21:18 -0400

    Updated format for multiple pages

--- frontend/clash-frontend/src/App.jsx
+++ frontend/clash-frontend/src/App.jsx
@@ -1,78 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+
 import './App.css'
 
-function Button({ label, onClick}) {
-	return (
-		<button className="button" onClick={onClick}>
-		  {label}
-		</button>
-	);
-}
-
-function MyComponent({ inputLabel = '', value, onChange }) {
-  return (
-    <div>
-      {inputLabel && <label htmlFor="inputBox">{inputLabel}</label>}
-      <input
-	id={inputLabel}
-        type="text"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      />
-    </div>
-  );
-}
-
-
 function App() {
-  const [playertag, setPlayertag] = useState('')
-  const [phone, setPhone] = useState('')
-  const [password, setPassword] = useState('')
-  const [otp, setOtp] = useState('')
-
-  const handleSubmit = async () => {
-	  const payload = {
-		  "tag": playertag,
-		  "phonenumber": phone,
-		  "password": password,
-		  "otp": otp
-	  };
-
-	  try {
-		  const response = await fetch('/signup', {
-			  method: 'POST',
-			  headers: {
-				  'Content-Type': 'application/json',
-			  },
-			  body: JSON.stringify(payload),
-		  });
-
-		  if (!response.ok) {
-			  throw new Error('Failed to submit data');
-		  }
-
-		  const data = await response.json();
-		  console.log('Success:', data);
-		} catch (error) {
-			console.error('Error:', error.message);
-		}
-
-  };
-
   return (
-    <>
-      <div className="SignupInfo">
-	<p><MyComponent inputLabel="Player Tag" value={playertag} onChange={setPlayertag}/></p>
-	<p><MyComponent inputLabel="Phone Number" value={phone} onChange={setPhone}/></p>
-	<p><MyComponent inputLabel="Password" value={password} onChange={setPassword}/></p>
-	<p><MyComponent inputLabel="API token" value={otp} onChange={setOtp}/></p>
-      </div>
-      <div>
-	<Button label="Submit" onClick={handleSubmit} />
-      </div>
-    </>
+    <Router>
+      <Routes>
+	<Route path="/" element={<LandingPage />} />
+	<Route path="/signup" element={<SignupPage />} />
+	<Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   )
 }
 
