61c9dcd0c4f13e2dbd53da69d16c05de0af3c9ef
RyanMConstable <RyanMConstable@gmail.com>
2025-05-21 17:56:08 -0400

    Added dashboard page

--- frontend/clash-frontend/src/App.jsx
+++ frontend/clash-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
 	<Route path="/" element={<LandingPage />} />
 	<Route path="/signup" element={<SignupPage />} />
 	<Route path="/login" element={<LoginPage />} />
+	<Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   )
