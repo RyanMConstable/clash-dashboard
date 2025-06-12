6961b44ab330d97b295b1d190e91a255a4e891ad
RyanMConstable <RyanMConstable@gmail.com>
2025-06-11 22:15:50 -0400

    Found enemy info from attack details

--- cron/eloCalculator.py
+++ cron/eloCalculator.py
@@ -1,8 +1,20 @@
 
 def playerElo(currentClanWarJSON, playerTag):
+    player = None
+    enemy = None
+
     #Work in progress
     for player in currentClanWarJSON["clan"]["members"]:
         if player["tag"] == playerTag:
             playerInfo = player
             break
+
+    if "attacks" in playerInfo:
+        for attack in playerInfo["attacks"]:
+            #attack["defenderTag"]
+            for enemy in currentClanWarJSON["opponent"]["members"]:
+                if enemy["tag"] == attack["defenderTag"]:
+                    enemyInfo = enemy
+
+    #HERE WE NEED TO CALCULATE ELO
     return
