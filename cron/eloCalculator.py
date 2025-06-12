5cdc5a39f935b76f97da7bf34dbc353cb3cba8ed
RyanMConstable <RyanMConstable@gmail.com>
2025-06-11 20:03:04 -0400

    Added ability to find player through a loop, will have to optimize in the future

--- cron/eloCalculator.py
+++ cron/eloCalculator.py
@@ -1,4 +1,8 @@
 
-def playerElo(currentClanWar, playerTag):
+def playerElo(currentClanWarJSON, playerTag):
     #Work in progress
+    for player in currentClanWarJSON["clan"]["members"]:
+        if player["tag"] == playerTag:
+            playerInfo = player
+            break
     return
