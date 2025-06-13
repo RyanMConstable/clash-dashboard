fb3ddbbf920e5a7d4828a9c99b537ed4606c18a0
RyanMConstable <RyanMConstable@gmail.com>
2025-06-13 14:24:05 -0400

    Adjusted function for addition of enemy info

--- cron/eloCalculator.py
+++ cron/eloCalculator.py
@@ -11,28 +11,26 @@ def playerElo(currentClanWarJSON, playerTag, attack):
             playerInfo = player
             break
 
-    if "attacks" in playerInfo:
-        for attack in playerInfo["attacks"]:
-            #attack["defenderTag"]
-            for enemy in currentClanWarJSON["opponent"]["members"]:
-                if enemy["tag"] == attack["defenderTag"]:
-                    enemyInfo = enemy
-                    break
+    #attack["defenderTag"]
+    for enemy in currentClanWarJSON["opponent"]["members"]:
+        if enemy["tag"] == attack["defenderTag"]:
+            enemyInfo = enemy
+            break
 
             #HERE WE NEED TO CALCULATE ELO
-            print(f"Player: {playerInfo}")
-            print(f"Enemy: {enemyInfo}")
+    print(f"Player: {playerInfo}")
+    print(f"Enemy: {enemyInfo}")
 
-            preferredAttackLow = playerInfo["mapPosition"] - 1
-            preferredAttackHigh = playerInfo["mapPosition"]
-            stars = attack["stars"]
-            destruction = attack["destructionPercentage"]
-            finishTime = attack["duration"]
-            enemyPosition = enemyInfo["mapPosition"]
-            enemyTH = enemyInfo["townhallLevel"]
-            TH = playerInfo["townhallLevel"]
+    preferredAttackLow = playerInfo["mapPosition"] - 1
+    preferredAttackHigh = playerInfo["mapPosition"]
+    stars = attack["stars"]
+    destruction = attack["destructionPercentage"]
+    finishTime = attack["duration"]
+    enemyPosition = enemyInfo["mapPosition"]
+    enemyTH = enemyInfo["townhallLevel"]
+    TH = playerInfo["townhallLevel"]
 
-            print(preferredAttackLow, preferredAttackHigh, stars, destruction, finishTime, enemyPosition, enemyTH, TH)
+    print(preferredAttackLow, preferredAttackHigh, stars, destruction, finishTime, enemyPosition, enemyTH, TH)
 
             
     return
