c786215d6bd9002387a79c475761bfd12f409212
RyanMConstable <RyanMConstable@gmail.com>
2025-06-13 14:43:50 -0400

    Added template for sections of calculating elo

--- cron/eloCalculator.py
+++ cron/eloCalculator.py
@@ -32,6 +32,92 @@ def playerElo(currentClanWarJSON, playerTag, attack):
 
     print(preferredAttackLow, preferredAttackHigh, stars, destruction, finishTime, enemyPosition, enemyTH, TH)
 
+    #Here is the elo calculation
+    eloChange = 0
+
+    if enemyPosition < preferredAttackLow:
+        #For attacking low targets
+        if TH == enemyTH:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+        elif TH > enemyTH:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+        else:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+    elif enemyPosition == preferredAttackLow or enemyPosition == preferredAttackHigh:
+        #For attacking preferred targets
+        if TH == enemyTH:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+        
+        elif TH > enemyTH:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+
+        else:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+    else:
+        #For attacking higher targets
+        if TH == enemyTH:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+        
+        elif TH > enemyTH:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+
+        else:
+            if stars == 3:
+
+            elif stars == 2:
+
+            elif stars == 1:
+
+            else:
+
     returnInfo["enemyPosition"] = enemyPosition
     returnInfo["enemyTH"] = enemyTH
     returnInfo["preferredAttackLow"] = preferredAttackLow
